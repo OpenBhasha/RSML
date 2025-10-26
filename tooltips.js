fb8bf23d52685abfe961cfcf481c5b99bb9e489e
Radhe Shyam Salopanthula <contact@imradhe.com>
2025-10-26 17:38:32 +0530

    Removec

--- tooltips.js
+++
@@ -1,7 +0,0 @@
-export function setupTagTooltips(root) {
-    const elements = root.querySelectorAll('[data-bs-toggle="tooltip"]');
-    elements.forEach(el => {
-      new bootstrap.Tooltip(el);
-    });
-  }
-  
